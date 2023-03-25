a689ede590df6ca0382622c34609142b98df4aa2
johinner <johimadacor@live.com>
2023-03-24 23:01:01 -0400

    documentacion con JsDoc

--- 04-blackjack-vite/src/blackjack/usecases/crear-deck.js
+++ 04-blackjack-vite/src/blackjack/usecases/crear-deck.js
@@ -1,10 +1,18 @@
 import _ from 'underscore';
 
-// Esta funcion crea una nueva baraja
+/**
+ * Esta funcion crea una nueva baraja
+ * @param {array<String>} tiposDeCarta Ejemplo: ["C", "D", "H", "S"]
+ * @param {array<String>} tiposEspeciales Ejemplo: ["J", "K", "Q", "A"]
+ * @returns {array} Retorna un arreglo de cartas
+ */
+
 export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+
+    if(!tiposDeCarta || !tiposEspeciales ||tiposDeCarta === 0) throw new Error('Tipos de carta y tipos Especioales son obligatorios')
     
     let deck = [];
-    
+
     for (let i = 2; i <= 10; i++) {
         for (let tipo of tiposDeCarta) {
             deck.push(i + tipo)
