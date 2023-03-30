661dc0fd650808d01cc291199b2d98cc00299143
johinner <johimadacor@live.com>
2023-03-29 21:16:12 -0400

    demo componente

---
+++ 06-advanced/src/concepts/01-environments.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+
+export const environmentsComponent = (element) => {
+    console.log(import.meta.env)
+    const html = `variable: `
+    element.innerHTML = html
+}
